a9f11ed8fdb9988c42b539be2086e77a848f51d9
Connor O'Hara <connor@switchboard.xyz>
2023-07-12 15:24:00 -0400

    map ports, start rollup in background, run test with retries

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
 FROM golang:1.20.3
 COPY script.sh /script.sh
 RUN chmod +x /script.sh
-ENTRYPOINT COSMOS_CHECKOUT=$COSMOS_CHECKOUT /script.sh
+ENTRYPOINT COSMOS_CHECKOUT=$COSMOS_CHECKOUT /bin/bash /script.sh
--- script.sh
+++ script.sh
@@ -56,7 +56,49 @@ DA_BLOCK_HEIGHT=$(curl http://celestia:26657/block | jq -r '.result.block.header
 echo $DA_BLOCK_HEIGHT
 
 # start the chain
+echo "Starting rollup in background!"
 gmd start --rollkit.aggregator true --rollkit.da_layer celestia --rollkit.da_config='{"base_url":"http://celestia:26659","timeout":60000000000,"fee":6000,"gas_limit":6000000}' --rollkit.namespace_id $NAMESPACE_ID --rollkit.da_start_height $DA_BLOCK_HEIGHT &
+
+URL=localhost:26657/block\?height=3
+EXPECTED_RESULT='{"jsonrpc":"2.0","error":{"code":-32603,"message":"","data":"failed to load hash from index: failed to load block hash for height: datastore: key not found"},"id":-1}'
+
+# Define the maximum number of retries
+MAX_RETRIES=50
+
+# Define the delay between retries in seconds
+RETRY_DELAY=5
+
+# Counter for the number of retries
+RETRY_COUNT=0
+
+# Loop until the result is not equal to the expected string or maximum retries are reached
+while [[ $RETRY_COUNT -lt $MAX_RETRIES ]]; do
+  # Execute the curl command and capture the result
+  RESULT=$(curl -s "$URL")
+
+  # Compare the result with the expected string or null string
+  if [[ "$RESULT" != "$EXPECTED_RESULT" && -n "$RESULT" ]]; then
+    echo "Success! The result is now different from the expected string or null string."
+    break
+  fi
+  echo "EXPECTED " $EXPECTED_RESULT
+  echo "GOT " $RESULT
+
+  # Increment the retry count
+  ((RETRY_COUNT++))
+
+  # Display a retry message
+  echo "Retrying... (Attempt $RETRY_COUNT)"
+
+  # Sleep for the specified delay before the next retry
+  sleep $RETRY_DELAY
+done
+
+# Check if maximum retries are reached without success
+if [[ $RETRY_COUNT -eq $MAX_RETRIES ]]; then
+  echo "Maximum retries reached. Unable to obtain a different result."
+fi
+
 
 ## uncomment the next command if you are using lazy aggregation
 ## gmd start --rollkit.aggregator true --rollkit.da_layer celestia --rollkit.da_config='{"base_url":"http://localhost:26659","timeout":60000000000,"fee":6000,"gas_limit":6000000}' --rollkit.namespace_id $NAMESPACE_ID --rollkit.da_start_height $DA_BLOCK_HEIGHT --rollkit.lazy_aggregator true
